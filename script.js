951df84faa8f4ca58b2c1ce9f45eb15add0e6dcf
HappaOfficial <103435413+Happa1145141919810@users.noreply.github.com>
2023-03-06 09:44:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+ const updateAccesssTokenURL = "https://auth.aliyundrive.com/v2/account/token"
+ const signinURL = "https://member.aliyundrive.com/v1/activity/sign_in_list"
+ const BOTURL = "https://api.telegram.org/bottoken/sendMessage?chat_id=-groupID-&text=阿里云盘已自动签到"
+ const refreshToeknArry = [
+     "在此处",
+     "填写你的刷新令牌"
+     ]
+ 
+ const fetch = require("node-fetch")
+ const notify = require('./sendNotify');
+ 
+ 
+ !(async() => {
+     for (const elem of refreshToeknArry) {
+         
+         const queryBody = {
+             'grant_type': 'refresh_token',
+             'refresh_token': elem
+         };
+ 
+         //使用 refresh_token 更新 access_token
+         fetch(updateAccesssTokenURL, {
+             method: "POST",    
+             body: JSON.stringify(queryBody),
+             headers: {'Content-Type': 'application/json'}
+         })
+         .then((res) => res.json())
+         .then((json) => {
+             // console.log(json);
+ 
+             let access_token = json.access_token;
+             console.log(access_token);
+             
+             
+             //签到
+             fetch(signinURL, {
+                 method: "POST",
+                 body: JSON.stringify(queryBody),
+                 headers: {'Authorization': 'Bearer '+access_token,'Content-Type': 'application/json'}
+             })
+             .then((res) => res.json())
+             .then((json) => {
+                 console.log(json);
+             })
+             .catch((err) => console.log(err))
+              fetch(BOTURL, {
+
+              })
+         })
+         .catch((err) => console.log(err))
+ 
+ 
+     }
+     // await notify.sendNotifyBark(`v2free 自动签到结果`,allnotify)
+     
+ 
+ })().catch((e) => {
+     console.error(`❗️  运行错误！\n${e}`)
+ }).finally()
+ // notify.sendNotify(`v2free 自动签到结果`,allnotify)
